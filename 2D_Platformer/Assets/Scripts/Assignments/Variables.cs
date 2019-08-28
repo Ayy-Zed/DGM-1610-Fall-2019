21dad18957f5e1665a19a2cb8458ca9da92ab9b7
Connor Christensen <connorwra3@gmail.com>
2019-08-28 17:03:58 -0600

    Added More variables

--- 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
+++ 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Variables : MonoBehaviour
 {
     string name = "Connor";
+    int age = 20;
+    float height = 6.1f;
+    bool married = false;
+    
 
 
 
     // Start is called before the first frame update
     void Start()
-    {
-        Debug.Log(name);
+    {   
+        Debug.Log("First Name: " + name);
+        Debug.Log("The Age of the Noble warrior: " + age);
+        Debug.Log("Not a short lad, he had a height of: " + height);
+        Debug.Log("Though a strapping young man, was he married?: " + married);
     }
 
    
