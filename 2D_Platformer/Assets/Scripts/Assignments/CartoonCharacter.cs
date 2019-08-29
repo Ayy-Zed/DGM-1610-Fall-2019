8722254918a547ca525bcf2dce559dd6e403335b
Connor Christensen <connorwra3@gmail.com>
2019-08-28 18:30:40 -0600

    Created 4 scripts for school assignment, SuperHero, SuperVillan, CartoonCharacter, and GameExperience

---
+++ 2D_Platformer/Assets/Scripts/Assignments/CartoonCharacter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string cartoonName = "Jack Ludacris";
+    string showName = "That boy's Ludacris";
+    int age = 14;
+    float height = 6.1f;
+    bool smart = True;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Debug.Log("Meet" + cartoonName);
+        Debug.Log("The new star of the hit cartoon show " + showName);
+        Debug.Log("It's a show about a boy who is only " + age);
+        Debug.Log("As tall as a normal boy " + height);
+        Debug.Log("True or Falso, Jack is a smart boy:  " + smart);
+    }
+
+   
+}
---
+++ 2D_Platformer/Assets/Scripts/Assignments/GameExperience.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string itemCrafted = "Sword";
+    string materialNeeded = "Stone";
+    int levelRequired = 7;
+    float expereienceRequired = 56.5f;
+    bool killsMobs = True;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Debug.Log("Are you ready to craft the " + itemCrafted);
+        Debug.Log("To craft this item, you need 15 " + materialNeeded);
+        Debug.Log("To craft this item, you must be at least level " + levelRequired);
+        Debug.Log("Make sure you have the required amount of expereience: " + expereienceRequired);
+        Debug.Log("True or Falso, This sword is capable of killing mobs:  " + killsMobs);
+    }
+
+   
+}
