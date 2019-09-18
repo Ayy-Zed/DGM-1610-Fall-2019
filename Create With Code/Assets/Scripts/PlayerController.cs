fbcc4353e6886338bbc73e2a1e96174507f183d3
Connor Christensen <connorwra3@gmail.com>
2019-09-18 17:04:00 -0600

    Finished Car Game - Added player input and rotation

--- Create With Code/Assets/Scripts/PlayerController.cs
+++ Create With Code/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    // Game private variables
+    private float speed = 20;
+    private float turnSpeed = 75;
+    private float horizontalInput;
+    private float forwardInput;
+    
     // Update is called once per frame
     void Update()
     {
-        // This code will make the vehicle move
-    
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // This code gives the player control
 
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // This code will make the vehicle move
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
   
     }
 }
