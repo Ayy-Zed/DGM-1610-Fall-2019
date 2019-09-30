0cba8cb07b969b9773894a6108fd3b37640f0eae
Connor Christensen <connorwra3@gmail.com>
2019-09-30 17:10:25 -0600

    Added Prototype 2
    Coded player controller for movement and constraints, also started code for launching pizza

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xrange = 10.0f;
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //These if statements keep players in bounds
+        if (transform.position.x < -xrange)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xrange)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+        //This code moves the player across the screen
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //This code tests for spacebar press to launch Pizza
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+
+    }
+}
