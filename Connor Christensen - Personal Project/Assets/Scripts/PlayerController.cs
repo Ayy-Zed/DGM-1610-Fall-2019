3b0ffeed7116f611ff39c5f3a72e46afa4b77a24
Connor Christensen <connorwra3@gmail.com>
2019-10-23 17:14:08 -0600

    Finished Challenge 3 including bonuses, and gave player ability to jump in personal project

---
+++ Connor Christensen - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    private float gravityModifier = 10;
+    private float jumpForce = 50;
+
+    private bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {   //Sets up player to be able to add jump force
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *=gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //If spacebar is pressed and player is on the ground then jump
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {//Only lets player jump when they touch the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
