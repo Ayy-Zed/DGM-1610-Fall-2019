9b92064b4d5170755bc3b289bee109427504c675
Connor Christensen <connorwra3@gmail.com>
2019-10-28 17:11:35 -0600

    Worked on personal game, could not figure out projectile but added basic scripts

--- Connor Christensen - Personal Project/Assets/Scripts/PlayerController.cs
+++ Connor Christensen - Personal Project/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public GameObject projectile;
 
     private float gravityModifier = 10;
     private float jumpForce = 50;
 
     private bool isOnGround = true;
+
     // Start is called before the first frame update
     void Start()
     {   //Sets up player to be able to add jump force
@@ -20,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   //If spacebar is pressed and player is on the ground then jump
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.W) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
             isOnGround = false;
@@ -32,6 +34,11 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectile, transform.position, projectile.transform.rotation);
         }
     }
 }
